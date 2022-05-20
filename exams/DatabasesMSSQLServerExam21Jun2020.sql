982c8588e04f90acfa5f0ba3e10e0e39ba26bc2d
Florieta <florieta.pencheva@gmail.com>
2022-05-20 21:38:53 +0300

    Added two more problem solution part of this exam

--- exams/DatabasesMSSQLServerExam21Jun2020.sql
+++ exams/DatabasesMSSQLServerExam21Jun2020.sql
@@ -188,3 +188,98 @@ INNER JOIN AccountsTrips ats
         ON cte.HotelTownId = r.HotelId
   ORDER BY [FullName],
            ats.TripId
+GO
+
+--Task 11
+
+CREATE FUNCTION udf_GetAvailableRoom (@HotelId INT, @Date DATE, @People INT)
+RETURNS NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @AllRooms TABLE (Id INT)
+    INSERT INTO @AllRooms 
+    SELECT DISTINCT r.[Id]
+               FROM [Rooms] AS r
+          LEFT JOIN [Trips] AS t
+                 ON t.[RoomId] = r.[Id]
+              WHERE r.[HotelId] = @HotelId
+                AND @Date BETWEEN t.[ArrivalDate] AND t.[ReturnDate]
+                AND t.[CancelDate] IS NULL
+
+    DECLARE @Room TABLE (Id INT, Price DECIMAL(15, 2), Type NVARCHAR(20), Beds INT, TotalPrice DECIMAL(15, 2))
+    INSERT INTO @Room
+    SELECT TOP 1
+               r.[Id],
+               r.[Price],
+               r.[Type],
+               r.[Beds],
+               (h.[BaseRate] + r.[Price]) * @People AS [TotalPrice]
+          FROM [Rooms] AS r
+     LEFT JOIN [Hotels] AS h
+            ON h.[Id] = r.[HotelId]
+         WHERE r.[HotelId] = @HotelId
+           AND r.[Beds] >= @People
+           AND r.[Id] NOT IN (SELECT [Id] FROM @AllRooms)
+      ORDER BY [TotalPrice] DESC
+
+    IF ((SELECT COUNT(*) FROM @Room) < 1)
+    BEGIN
+        RETURN 'No rooms available'
+    END
+
+    DECLARE @Result NVARCHAR(MAX)
+    SET @Result = (SELECT CONCAT('Room ', r.[Id], ': ', r.[Type], ' (', r.[Beds], ' beds) - $', r.[TotalPrice]) FROM @Room r)
+
+    RETURN @Result
+END
+GO
+
+--Task 12
+
+CREATE PROC usp_SwitchRoom(@TripId INT, @TargetRoomId INT)
+AS
+BEGIN
+    DECLARE @OldHotelId INT
+    SET @OldHotelId = (    SELECT h.Id
+                             FROM Hotels h
+                       INNER JOIN Rooms r
+                               ON r.HotelId = h.Id
+                       INNER JOIN Trips t
+                               ON t.RoomId = r.Id
+                            WHERE t.id = @TripId)
+
+    DECLARE @NewHotelId INT
+    SET @NewHotelId = (    SELECT h.Id
+                             FROM Hotels h
+                       INNER JOIN Rooms r
+                               ON r.HotelId = h.Id
+                            WHERE r.Id = @TargetRoomId)
+
+    IF (@OldHotelId <> @NewHotelId)
+    BEGIN
+        RAISERROR('Target room is in another hotel!', 16, 1)
+        RETURN
+    END
+
+    DECLARE @TripAccounts INT
+    SET @TripAccounts = (SELECT COUNT(ats.TripId) 
+                           FROM AccountsTrips ats 
+                          WHERE ats.TripId = @TripId)
+
+    DECLARE @RoomBeds INT
+    SET @RoomBeds = (SELECT r.Beds 
+                       FROM Rooms r 
+                      WHERE r.Id = @TargetRoomId)
+
+    IF (@TripAccounts > @RoomBeds)
+    BEGIN
+        RAISERROR('Not enough beds in target room!', 16, 1)
+        RETURN
+    END
+
+    BEGIN TRANSACTION
+        UPDATE Trips
+           SET RoomId = @TargetRoomId
+         WHERE Id = @TripId
+    COMMIT
+END
